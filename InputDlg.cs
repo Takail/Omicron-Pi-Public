bea1b8a949905de49f4eb7be300058dd81cb9013
Takail <hancockjake1@gmail.com>
2020-04-21 02:04:08 +0100

    Initial Release
    Initial Release

---
+++ InputDlg.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using YamlDotNet.Core.Tokens;
+
+namespace Omicron_Pi
+{
+    public partial class InputDlg : Form
+    {
+        public InputDlg()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Globals.inputResult = textBox1.Text.ToLower();
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            Close();
+        }
+
+        private void NewNameDlg_Load(object sender, EventArgs e)
+        {
+            if (Globals.inputType == "role")
+            {
+                Text = "Please enter a new role name";
+                label1.Text = "Please enter a new role name";
+                textBox1.Text = Globals.inputResult;
+            }
+            else
+            {
+                Text = "Please enter a new userid";
+                label1.Text = "Please enter a new userid";
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == 13)
+            {
+                e.Handled = true;
+                Globals.inputResult = textBox1.Text.ToLower();
+                DialogResult = DialogResult.OK;
+                Close();
+            }
+        }
+    }
+}
---
+++ SplashScreen.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace Omicron_Pi
+{
+    partial class SplashScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openExistingConfigButton = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // openExistingConfigButton
+            // 
+            this.openExistingConfigButton.Location = new System.Drawing.Point(12, 12);
+            this.openExistingConfigButton.Name = "openExistingConfigButton";
+            this.openExistingConfigButton.Size = new System.Drawing.Size(176, 27);
+            this.openExistingConfigButton.TabIndex = 1;
+            this.openExistingConfigButton.Text = "Open Existing Config";
+            this.openExistingConfigButton.UseVisualStyleBackColor = true;
+            this.openExistingConfigButton.Click += new System.EventHandler(this.openExistingConfigButton_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 45);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(176, 27);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Create New Config";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "config_remoteadmin.txt";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // SplashScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(208, 84);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.openExistingConfigButton);
+            this.Name = "SplashScreen";
+            this.Text = "SplashScreen";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.SplashScreen_FormClosed);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button openExistingConfigButton;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
